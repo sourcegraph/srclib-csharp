3adc9b9461893d22ae84c2b12ef4cbdf5bbe338c
isaev <Ildar Isaev>
2016-03-30 16:48:36 +0300

    .csproj -> project.json conversion

--- Srclib.Nuget/ConsoleCommands/ScanConsoleCommand.cs
+++ Srclib.Nuget/ConsoleCommands/ScanConsoleCommand.cs
@@ -4,11 +4,74 @@ using System.Collections.Generic;
 using Microsoft.Dnx.Runtime;
 using Microsoft.Dnx.Runtime.Common.CommandLine;
 using Newtonsoft.Json;
+using System.Linq;
 
 namespace Srclib.Nuget
 {
   class ScanConsoleCommand
   {
+
+
+        public static void ConvertCsproj(string path)
+        {
+            PackageVersions pv = new PackageVersions();
+            HashSet<string> names = new HashSet<string>();
+            string dir = path.Substring(0, path.LastIndexOf('/'));
+            string input = File.ReadAllText(path);
+            string output = "{\n  \"frameworks\": {\n    \"dnx451\": {\n      \"dependencies\": {\n";
+            int idx = input.IndexOf("<Reference Include=\"");
+            while (idx != -1)
+            {
+                int quote = input.IndexOf('\"', idx + 20);
+                int comma = input.IndexOf(',', idx + 20);
+                if ((comma != -1) && (comma < quote))
+                {
+                    quote = comma;
+                }
+                string name = input.Substring(idx + 20, quote - idx - 20);
+                if (!names.Contains(name))
+                {
+                    names.Add(name);
+                    string version = pv.GetVersion(name.ToLower());
+                    if (version != null)
+                    {
+                        output = output + "          \"" + name + "\": \"" + version + "\",\n";
+                    }
+                    else
+                    {
+                        output = output + "          \"" + name + "\": \"\",\n";
+                    }
+                }
+                idx = input.IndexOf("<Reference Include=\"", quote);
+            }
+            output = output + "      }\n    }\n  }\n}";
+            File.WriteAllText(dir + "/project.json", output);
+        }
+
+        public static FileInfo[] FindVSProjects(DirectoryInfo root)
+        {
+            FileInfo[] files = null;
+            DirectoryInfo[] subDirs = null;
+
+            try
+            {
+                files = root.GetFiles("*.csproj");
+            }
+            catch (Exception e)
+            {
+            }
+
+            subDirs = root.GetDirectories();
+            foreach (DirectoryInfo dirInfo in subDirs)
+            {
+                // Resursive call for each subdirectory.
+                FileInfo[] res = FindVSProjects(dirInfo);
+                files = files.Concat(res).ToArray();
+            }
+            return files;
+        }
+
+
     public static void Register(CommandLineApplication cmdApp, Microsoft.Extensions.PlatformAbstractions.IApplicationEnvironment appEnvironment)
     {
       cmdApp.Command("scan", c => {
@@ -19,6 +82,16 @@ namespace Srclib.Nuget
         c.OnExecute(async () => {
           var dir = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "."));
 
+            DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory());
+            FileInfo[] fis = FindVSProjects(di);
+            string[] projects = new string[fis.Length];
+            for (int i = 0; i < fis.Length; i++)
+            {
+                projects[i] = fis[i].FullName;
+                ConvertCsproj(projects[i]);
+            }
+
+
           var sourceUnits = new List<SourceUnit>();
           foreach(var proj in Scan(dir))
           {
@@ -27,7 +100,7 @@ namespace Srclib.Nuget
 
           if (sourceUnits.Count == 0)
           {
-            //not a DNX project
+            //not a DNX project and not a VS project
             sourceUnits.Add(SourceUnit.FromDirectory("name", dir));
           }
 
